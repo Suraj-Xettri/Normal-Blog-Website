e710f0c42b7b29ddf46b6bd54df1b2e4c13b34b9
Suraj Thapa <surajthapart@gmail.com>
2024-10-03 20:13:30 +0545

    added all the components to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import BlogList from "./components/BlogList"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import Hero from "./components/Hero"
+
 function App() {
   return (
     <div className="bg-gray-100 min-h-screen">
+    <Header/>
+    <Hero />
     <BlogList />
+    <Footer/>
   </div>
   )
 }
